086fb9e6341e4497d1695efdb24abd78dab86846
Omar Sader <omar.sader@gmail.com>
2018-08-05 12:42:57 -0400

    Higher Order Functions

--- Higher Order Functions/index.js
+++ Higher Order Functions/index.js
@@ -3,3 +3,9 @@ function sing(){
     console.log("how i wonder...");
 }
 
+setInterval(sing, 1000) - gives you a number to stop.
+clearInterval() - stops Interval
+setInterval(function(){
+    console.log("I am an anonymous function!");
+    console.log("This is awesome!");   
+}, 2000);
